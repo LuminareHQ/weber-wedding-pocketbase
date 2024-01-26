d31a678e53ccae883e4c2203f5c63292b66a5334
LuminareHQ <70502610+LuminareHQ@users.noreply.github.com>
2024-01-26 15:43:38 -0500

    update rsvp and server offline handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="flex flex-col w-full h-full items-center justify-center gap-0.5">
-	<h1 class="h2">The Factory Events</h1>
-	<p>1546 Knowlton Street</p>
-	<p>Cincinnati, Ohio 45223</p>
+    <h1 class="h2">The Factory Events</h1>
+    <address>1546 Knowlton Street<br>
+        Cincinnati, Ohio 45223
+    </address>
 </div>
